ae9e0897ce7b993fb94274fb9c9887f4e2b6dbcb
Sonnuy12 <kazanskij516@gmail.com>
2024-12-16 18:57:59 +0300

    Initial Commit

---
+++ Проверка_Авторизация/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Проверка_Авторизация
+//
+//  Created by сонный on 16.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
