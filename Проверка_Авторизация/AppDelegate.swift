c01165527fcf5da56eef0c078016d688d64a7ac3
Sonnuy12 <kazanskij516@gmail.com>
2024-12-17 13:23:21 +0300

    Создание и подключения кнопки авторизации VK SDK

--- Проверка_Авторизация/AppDelegate.swift
+++ Проверка_Авторизация/AppDelegate.swift
@@ -5,17 +5,51 @@
 //  Created by сонный on 16.12.2024.
 //
 
+
 import UIKit
+import VKID
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    //шаг второй 2
+    var vkid: VKID?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //Шаг первый 1
         // Override point for customization after application launch.
+        do {
+            self.vkid = try VKID(
+                config: Configuration(
+                    appCredentials: AppCredentials(
+                        clientId: "52851134",
+                        clientSecret: "eAVPARleqv1sg8o53F7S"
+                    ),
+                    loggingEnabled: false
+                )
+            )
+        } catch {
+            preconditionFailure("Failed to initialize VKID: \(error)")
+        }
         return true
+        
     }
+    //шаг второй 2
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+    ) -> Bool {
+        // Переадресация URL в VKID
+        return vkid?.open(url: url) ?? false
+    }
+//    func application(_ app: UIApplication,
+//                     open url: URL,
+//                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+//        // Перехват URL и передача в VKID
+//        return vkid?.open(url: url) ?? false
+//    }
+//  
 
     // MARK: UISceneSession Lifecycle
 
@@ -30,7 +64,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
--- Проверка_Авторизация/ViewController.swift
+++ Проверка_Авторизация/ViewController.swift
@@ -4,16 +4,72 @@
 //
 //  Created by сонный on 16.12.2024.
 //
-
 import UIKit
+import VKID
+import VKIDCore
 
 class ViewController: UIViewController {
+    var vkid: VKID! // Убедитесь, что vkid передается при инициализации
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createBackround()
+        //view.backgroundColor = .white
+        
+        setupVKIDButton()
     }
 
+    private func setupVKIDButton() {
+        // Создаем кнопку "One Tap"
+        let oneTap = OneTapButton(
+            appearance: .init(
+                title: .open,
+                style: .primary(logo: .vkidPrimary),
+                theme: .matchingColorScheme(.light)
+            ),
+            layout: .regular(
+                height: .large(.h56),
+                cornerRadius: 28
+            ),
+            presenter: .newUIWindow // Отображение кнопки как модального окна
+        ) { [weak self] authResult in
+            self?.handleAuthResult(authResult)
+        }
 
+        // Создаем UI элемент кнопки
+        let oneTapView = vkid.ui(for: oneTap).uiView()
+        oneTapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Добавляем на экран
+        view.addSubview(oneTapView)
+        
+        // Настраиваем автолейаут
+        NSLayoutConstraint.activate([
+            oneTapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            oneTapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            oneTapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            oneTapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            oneTapView.heightAnchor.constraint(equalToConstant: 56)
+        ])
+    }
+
+    private func handleAuthResult(_ authResult: Result<UserSession, AuthError>) {
+        // Обработка результата авторизации
+        switch authResult {
+        case .success(let session):
+            print("Авторизация успешна! Token: \(session.accessToken), User ID: \(String(describing: session.user))")
+        case .failure(AuthError.cancelled):
+            print("Авторизация отменена пользователем")
+        case .failure(let error):
+            print("Ошибка авторизации: \(error.localizedDescription)")
+        }
+    }
+    
+    func createBackround() {
+        let backgroundImageView = UIImageView(frame: self.view.bounds)
+        backgroundImageView.image = UIImage(named: "BackroundAuthorization")
+        backgroundImageView.contentMode = .scaleAspectFill
+        self.view.addSubview(backgroundImageView)
+        self.view.sendSubviewToBack(backgroundImageView)
+    }
 }
-
